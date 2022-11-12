32975d542d08354d23d775569e1041b103db1b45
Muhammad Hamza <m.waqas.hamza@gmail.com>
2022-11-12 19:00:38 +0500

    add material ui theme

---
+++ client/src/theme/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#181818',
+    },
+    secondary: {
+      main: '#FB2576',
+    },
+  },
+});
+
+export default theme;
