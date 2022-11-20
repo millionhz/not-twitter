a44d704464b1df6b891136e244b9a67a30c5deaf
Humaira Fasih <humairafasih18@gmail.com>
2022-11-20 12:36:48 +0500

    Add Protected Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,16 +6,23 @@ import LogInPage from './features/authentication/LogInPage';
 import SignUpPage from './features/authentication/SignUpPage';
 import HomePage from './features/home/HomePage';
 import ProfilePage from './features/profile/ProfilePage';
+import ProtectedRoutes from './features/authentication/ProtectedRoutes';
+import PageNotFound from './features/not-found/PageNotFound';
+import CreatePostPage from './features/post/CreatePostPage';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Routes>
         <Route path="/">
-          <Route path="home" element={<HomePage />} />
-          <Route path="myprofile" element={<ProfilePage />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="home" element={<HomePage />} />
+            <Route path="home/createpost" element={<CreatePostPage />} />
+            <Route path="myprofile" element={<ProfilePage />} />
+          </Route>
           <Route path="login" element={<LogInPage />} />
           <Route path="signup" element={<SignUpPage />} />
+          <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
     </ThemeProvider>
