8c22dc69cb7a306cbaea011faf52603e0141388f
harris-ahmad <24100315@lums.edu.pk>
2022-11-27 03:17:42 +0500

    code logic for post router improved

--- server/routes/api/signup.js
+++ server/routes/api/signup.js
@@ -1,23 +1,40 @@
-const express = require('express');
-const { insertUser } = require('../../utilities/database');
-
+const express = require("express");
+const { insertUser } = require("../../utilities/database");
+const { hasAllFields, constraints } = require("../../utilities/hasAllFields");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Sign up' });
+router.get("/", (req, res) => {
+  res.json({ message: "Sign up" });
 });
 
-router.post('/', (req, res, next) => {
-  const { email, password } = req.body;
-
-  insertUser(email, password)
-    .then(() => {
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err.message });
-      next(err);
-    });
+router.post("/", (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validInput = hasAllFields(
+      {
+        name: constraints.name,
+        email: constraints.email,
+        password: constraints.password,
+        dob: constraints.dob,
+      },
+      req.body
+    );
+    if (validInput !== true) {
+      res.status(400).send(validInput);
+    } else {
+      insertUser(email, password)
+        .then(() => {
+          res.sendStatus(200);
+        })
+        .catch((err) => {
+          res.status(500).json({ message: err.message });
+          next(err);
+        });
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
