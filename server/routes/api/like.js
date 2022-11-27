1b5101cea34547567ef88c68e0ecf1935249e587
Muhammad Hamza <m.waqas.hamza@gmail.com>
2022-11-27 13:51:36 +0500

    implement like using promise chaining

--- server/routes/api/like.js
+++ server/routes/api/like.js
@@ -20,24 +20,29 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', (req, res, next) => {
   const { user_id: userId } = req.user;
   const { postId } = req.body;
 
-  try {
-    await insertLike(postId, userId);
-    res.sendStatus(200);
-  } catch (err) {
-    if (err.errno === 1062) {
-      try {
-        await deleteLike(postId, userId);
-        res.sendStatus(200);
-      } catch (err) {
+  insertLike(postId, userId)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      if (err.errno !== 1062) {
         res.status(500).json({ message: err.message });
         next(err);
+      } else {
+        deleteLike(postId, userId)
+          .then(() => {
+            res.sendStatus(200);
+          })
+          .catch((err_) => {
+            res.status(500).json({ message: err_.message });
+            next(err_);
+          });
       }
-    }
-  }
+    });
 });
 
 module.exports = router;
