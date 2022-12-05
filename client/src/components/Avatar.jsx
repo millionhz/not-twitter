429f99172b96f0d35986e37f1c568b1097a12509
Muhammad Hamza <m.waqas.hamza@gmail.com>
2022-12-05 21:08:25 +0500

    remove unused var

--- client/src/components/Avatar.jsx
+++ client/src/components/Avatar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Avatar } from '@mui/material';
 
-function Avatar_({ name, image, sx }) {
+function Avatar_({ name, sx }) {
   return (
     <Avatar sx={{ ...sx, bgcolor: 'primary.main' }}>
       {name ? name[0].toUpperCase() : ''}
