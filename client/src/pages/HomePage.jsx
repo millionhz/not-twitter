bf3464e1eb8c51b61e2b4ee3965695163b51f175
Humaira Fasih <humairafasih18@gmail.com>
2022-12-10 05:11:24 +0500

    refactor homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -12,23 +12,11 @@ import {
   ListItemButton,
   ListItemText,
 } from '@mui/material';
-import {
-  Key,
-  Search,
-  PostAdd,
-  Person,
-  Notifications,
-  Upload,
-  Edit,
-  ManageAccounts,
-  ReportProblem,
-  Logout,
-} from '@mui/icons-material';
 
 import { getPosts } from '../api/backend';
 import PostList from '../components/PostList';
 import AuthContext from '../context/AuthContext';
-// import InfiniteScroll from 'react-infinite-scroll-component';
+import sideBarNav from '../components/SideBarNav';
 
 function HomePage() {
   const [posts, setPosts] = useState([]);
@@ -37,64 +25,7 @@ function HomePage() {
     user: { userId, isAdmin },
   } = useContext(AuthContext);
 
-  const drawerWidth = 240;
-  const sideBarNav = [
-    {
-      route: `/user/${userId}`,
-      icon: <Person sx={{ margin: 1 }} />,
-      label: 'My Profile',
-    },
-    {
-      route: '/user/edit',
-      icon: <Edit sx={{ margin: 1 }} />,
-      label: 'Edit Profile',
-    },
-    {
-      route: '/notifications',
-      icon: <Notifications sx={{ margin: 1 }} />,
-      label: 'Notifications',
-    },
-    {
-      route: '/post/compose',
-      icon: <PostAdd sx={{ margin: 1 }} />,
-      label: 'Create Post',
-    },
-    {
-      route: '/image/compose',
-      icon: <Upload sx={{ margin: 1 }} />,
-      label: 'Upload Image',
-    },
-    {
-      route: '/post/search',
-      icon: <Search sx={{ margin: 1 }} />,
-      label: 'Search Post',
-    },
-    {
-      route: '/user/search',
-      icon: <Search sx={{ margin: 1 }} />,
-      label: 'Search User',
-    },
-    {
-      route: '/user/manage',
-      icon: <ManageAccounts sx={{ margin: 1 }} />,
-      label: 'Manage Users',
-    },
-    {
-      route: '/post/report',
-      icon: <ReportProblem sx={{ margin: 1 }} />,
-      label: 'Manage Posts',
-    },
-    {
-      route: '/updatepassword',
-      icon: <Key sx={{ margin: 1 }} />,
-      label: 'Update Password',
-    },
-    {
-      route: '/logout',
-      icon: <Logout sx={{ margin: 1 }} />,
-      label: 'Logout',
-    },
-  ];
+  const drawerWidth = 250;
 
   useEffect(() => {
     getPosts()
@@ -113,14 +44,18 @@ function HomePage() {
   }, []);
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box
+      sx={{
+        display: 'flex',
+      }}
+    >
       <CssBaseline />
       <AppBar
         position="fixed"
         sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
       >
         <Toolbar sx={{ display: 'flex', flexDirection: 'row' }}>
-          <Typography variant="h4" noWrap sx={{ flex: 1 }}>
+          <Typography textAlign="center" variant="h3" noWrap sx={{ flex: 1 }}>
             Dostiyan
           </Typography>
         </Toolbar>
@@ -139,7 +74,7 @@ function HomePage() {
         <Toolbar />
         <Box sx={{ overflow: 'auto' }}>
           <List>
-            {sideBarNav.map(({ route, icon, label }) =>
+            {sideBarNav(userId).map(({ route, icon, label }) =>
               !isAdmin &&
               (label === 'Manage Users' || label === 'Manage Posts') ? null : (
                 <ListItem key={label} disablePadding>
@@ -153,7 +88,7 @@ function HomePage() {
                   >
                     <ListItemButton>
                       {icon}
-                      <ListItemText primary={label} />
+                      <ListItemText primary={label} sx={{ width: 177 }} />
                     </ListItemButton>
                   </Link>
                 </ListItem>
@@ -162,7 +97,10 @@ function HomePage() {
           </List>
         </Box>
       </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, paddingLeft: '18%', paddingY: 3 }}
+      >
         <Toolbar />
         <PostList posts={posts} />
       </Box>
